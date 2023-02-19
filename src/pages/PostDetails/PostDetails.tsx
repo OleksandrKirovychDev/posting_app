af58cdfc396cd2671fe861e23fb71b4c71f85286
OleksandrKirovychDev <oleksandr.kirovych.dev@gmail.com>
2023-02-19 16:54:28 +0200

    Animation for postDetails

--- src/pages/PostDetails/PostDetails.tsx
+++ src/pages/PostDetails/PostDetails.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/system';
-import { Paper, Typography, CircularProgress, Divider } from '@mui/material';
+import { Paper, Typography, Divider, Grow } from '@mui/material';
 import useStyles from './styles';
 
 import CommentSection from '../../components/CommentSection/CommentSection';
@@ -29,29 +29,29 @@ const PostDetails: React.FC = () => {
 
   const { post } = useAppSelector((state) => state.postDetails);
   const { comments } = useAppSelector((state) => state.postDetails);
-  const { isLoading } = useAppSelector((state) => state.postDetails);
-  return !isLoading ? (
+
+  return (
     <Box width="100%" display="flex" justifyContent="center">
-      <Paper
-        style={{ padding: '20px', maxWidth: '1500px', flexGrow: '1' }}
-        elevation={6}
-      >
-        <div className={cx(classes.card)}>
-          <div className={cx(classes.section)}>
-            <Typography variant="h4" component="h3">
-              {post?.title}
-            </Typography>
-            <Typography gutterBottom variant="body1" component="p">
-              {post?.body}
-            </Typography>
-            <Divider style={{ margin: '20px 0' }} />
+      <Grow in>
+        <Paper
+          style={{ padding: '20px', maxWidth: '1500px', flexGrow: '1' }}
+          elevation={6}
+        >
+          <div className={cx(classes.card)}>
+            <div className={cx(classes.section)}>
+              <Typography variant="h4" component="h3">
+                {post?.title}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="p">
+                {post?.body}
+              </Typography>
+              <Divider style={{ margin: '20px 0' }} />
+            </div>
           </div>
-        </div>
-        <CommentSection comments={comments} post={post!} />
-      </Paper>
+          <CommentSection comments={comments} post={post!} />
+        </Paper>
+      </Grow>
     </Box>
-  ) : (
-    <CircularProgress />
   );
 };
 
